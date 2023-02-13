02a8fcd3269d5fff59c97c4bcf3fe4a5c578ee86
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-14 00:46:34 +0530

    Add : Solved UserName_BasicProblem_1

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿namespace Program
+{
+    public class UserName
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Enter the Name : ");
+            string n = Console.ReadLine();
+            Console.WriteLine("Hello " + n);
+        }
+    }
+}

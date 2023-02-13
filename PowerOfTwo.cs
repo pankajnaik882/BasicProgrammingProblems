82cf95b5bc7c863f9cc19f235cd5bb30cd059482
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-14 00:50:09 +0530

    Add : Solved PowerOfTwo_BasicProblem_3

---
+++ PowerOfTwo.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public class PowerOfTwo
+    {
+        public static void tableoftwo()
+        {
+            int i;
+            int num = 2;
+            int N = 31;
+            for (i = 0; i <= N; i++)
+            {
+                double pow = Math.Pow(num, i);
+                Console.WriteLine("Calculated 2 power" + i + ":-" + pow);
+            }
+        }
+    }
+}

5330bff7941b5b6ad3c85c0b8e1e02fd33926ce9
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-14 00:48:23 +0530

    Add : Solved LeapYear_BasicProblem_2

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿namespace Program
+﻿using BasicProblems;
+
+namespace Program
 {
     public class UserName
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Enter the Name : ");
-            string n = Console.ReadLine();
-            Console.WriteLine("Hello " + n);
+            LeapYear.display();
         }
     }
 }

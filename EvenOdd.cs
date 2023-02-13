3463c5e8c49456daf761aed3a0534a3af84985d1
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-14 01:00:33 +0530

    Add : Solved EvenOdd_BasicProgram_8

---
+++ EvenOdd.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Program
+{
+    public class EvenOdd
+    {
+        public static void CheckEvenOdd()
+        {
+            int num;
+            Console.WriteLine("Enter a Number To check Even Or Odd : ");
+            num = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(num);
+            if (num % 2 == 0)
+            {
+                Console.WriteLine("Even Number : " + num);
+
+
+            }
+            else
+            {
+                Console.WriteLine("Odd Number : " + num);
+            }
+        }
+    }
--- Program.cs
+++ Program.cs
@@ -4,7 +4,7 @@
     {
         public static void Main(string[] args)
         {
-            SwapTwoNumber.SwapNumber();
+            EvenOdd.CheckEvenOdd();
         }
     }
 }

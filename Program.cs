21eb7ae7c2f4a71798c1d5042ec6c204ebaf7f4f
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-14 00:54:53 +0530

    Add : Solved PrimeFactor_BasicProblem_5

--- Program.cs
+++ Program.cs
@@ -1,12 +1,12 @@
-﻿using BasicProblems;
-
-namespace Program
+﻿namespace Program
 {
     public class UserName
     {
         public static void Main(string[] args)
         {
-            Harmonic.HarmonicNumber();
+            Console.WriteLine("Please Enter Number to find Factor");
+            int prime = Convert.ToInt32(Console.ReadLine());
+            Prime.PrimeFactors(prime);
         }
     }
 }

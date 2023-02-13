a6118db5a844823e26e916fb0a82dd4a4bedbf96
“pankajnaik882” <pankaj080519999@gmail.com>
2023-02-14 00:53:00 +0530

    Add : Solved Harmonic_BasicProblem_4

---
+++ Harmonic.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicProblems
+{
+    public class Harmonic
+    {
+        public static void HarmonicNumber()
+        {
+            Console.WriteLine("Enter the a number for harmonic");
+            int N = Convert.ToInt32(Console.ReadLine());
+            double first = 0.0;
+
+            if (N > 0)
+            {
+                Console.WriteLine("Hii");
+                for (int i = 1; i < N; i++)
+                {
+                    double temp = i;
+                    first = first + (1.0 / i);
+                    Console.WriteLine("Harmonic : " + first);
+                }
+            }
+            else
+            {
+                Console.WriteLine("Please Enter the value greater than 0 ");
+            }
+        }
+    }
+}
